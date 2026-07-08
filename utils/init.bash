2cccc624a46ed27be479c4bbe57af6648d67d501
Aavin Fernando <59203114+Avnzx@users.noreply.github.com>
2026-07-08 14:23:08 +1000

    X ros fixes (#206)
    * Fix dependencies in Dockerfile, add installnvim script per Jethro's request, fix init.bash to actually work :sob:
    
    * Move command index to owr-docs ros2 page (SST)

--- utils/init.bash
+++ utils/init.bash
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Run without args to set up your terminal environment
+# Source without args to set up your terminal environment
 # Run `./init.bash owr_update` to run rosdep
-function owr_run_subshells {
-    if [ $SHLVL -le 10 ]; then
-        /bin/bash -c "$0 owr_run_subshells"
-        /bin/bash; echo "WARN: Exited subshell $SHLVL"
-    else 
-        echo "Initialized!"
-    fi
-}
 
+# You have to manually run this!
 function owr_update {
     rosdep update && rosdep install --from-paths src --ignore-src -y
 }
 
 if [[ $# -gt 0 ]]; then "$@"; exit; fi      # Run func named by first arg
 source /ros_entrypoint.sh
-owr_run_subshells
